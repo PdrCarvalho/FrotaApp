012bc05523def2514a6c20a99906aaa252d2182b
carvalhao <carvalho.pedro.cardoso@gmail.com>
2019-02-06 18:21:25 -0300

    vdc

---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React, {componet} from "react"
+import {View,Text} from "react-native"
+export default class Main extends componet {
+    static navigationOptions = {
+        title:'Frota Inteligente'
+    }
+    render(){
+        return (
+            <View><Text>FrotaApp</Text></View>
+        )
+    }   
+}
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Main from './pages/main';
+/* export default createStackNavigator ({
+     Main,
+ });
+*/
+const AppNavigation 
+//https://reactnavigation.org/docs/en/hello-react-navigation.html
